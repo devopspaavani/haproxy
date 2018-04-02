69ce94eed8d7205da8fcfd621f72b7dbcc364bf0
devopspaavani <devopspaavani@gmail.com>
2018-04-02 18:53:48 -0500

    created haproxy.cfg template

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'haproxy'
+
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
+end
+
+service 'haproxy' do
+  action [:start, :enable] 
+end
+
