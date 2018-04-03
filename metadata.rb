2f71042b08d650c5ca648ec7162aef2059b82326
devopspaavani <devopspaavani@gmail.com>
2018-04-02 23:24:43 -0500

    update cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures haproxy'
 long_description 'Installs/Configures haproxy'
-version '0.1.0'
+version '0.2.0'
+depends 'apache', '>= 0.3.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
+
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
